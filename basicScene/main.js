e8e77320c2dcd83fde567ac5ae3e1ecbb61da0bb
GEXER007 <ni990527@tuta.io>
2022-12-04 12:23:29 +0100

    basicScene

--- basicScene/main.js
+++ basicScene/main.js
@@ -24,13 +24,13 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({color: 0xff0000})
 const mesh = new THREE.Mesh(geometry, material)
 
-scene.add(mesh)
+scene.add(mesh) // This is a method
 
 /*
     Sizes & Camera
 */
 // Sizes
-const sizes = {
+const sizes = { //This is an object.
     width: 800,
     height: 600
 }
@@ -44,5 +44,5 @@ scene.add(camera)
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+renderer.setSize(sizes.width, sizes.height)  // This is a method
+renderer.render(scene, camera)  // This is a method
